c4ac45d69102ac31c2205f226896c7347fc7c8fa
Qu Kaidi <1440463586@qq.com>
2019-04-13 17:02:46 +0800

    table

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,41 +1,75 @@
 <template>
   <div class="app-container">
+    <el-row type="flex" justify="end">
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="2">
+        <div>
+          <el-button @click="centerDialogVisible = true">添加视频</el-button>
+
+          <el-dialog title="提示" :visible.sync="centerDialogVisible" width="30%" center>
+            <el-form>
+              <el-form-item>
+                <el-upload
+                  class="avatar-uploader"
+                  ref="upload"
+                  :action="upload_url"
+                  list-type="picture-card"
+                  :name="upload_name"
+                  :on-remove="handleRemove"
+                  :on-exceed="handleExceed"
+                  :file-list="ad_url_list"
+                  :limit="1"
+                  :http-request="uploadSectionFile"
+                >
+                  <span class="font-14">选择图片或视频</span>
+                  <div slot="tip" class="el-upload__tip">尺寸750*1125px，大小2M以内，视频支持MP4</div>
+                </el-upload>
+              </el-form-item>
+              <el-form-item label="标题">
+                <el-input></el-input>
+              </el-form-item>
+              <el-form-item label="介绍">
+                <el-input type="textarea"></el-input>
+              </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+              <el-button @click="centerDialogVisible = false">取 消</el-button>
+              <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+            </span>
+          </el-dialog>
+        </div>
+      </el-col>
+    </el-row>
+    <br>
     <el-table
       v-loading="listLoading"
       :data="list"
       element-loading-text="Loading"
       border
       fit
-      highlight-current-row>
+      highlight-current-row
+    >
       <el-table-column align="center" label="ID" width="95">
+        <template slot-scope="scope">{{ scope.$index }}</template>
+      </el-table-column>
+      <el-table-column label="标题">
+        <template slot-scope="scope">{{ scope.row.title }}</template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="时间" width="200">
         <template slot-scope="scope">
-          {{ scope.$index }}
+          <i class="el-icon-time"/>
+          <span>{{ scope.row.display_time }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="用户名" width="110" align="center">
         <template slot-scope="scope">
           <span>{{ scope.row.author }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="操作" width="250" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time"/>
-          <span>{{ scope.row.display_time }}</span>
+          <span>
+            <el-button @click="del">删除</el-button>
+          </span>
         </template>
       </el-table-column>
     </el-table>
@@ -43,36 +77,63 @@
 </template>
 
 <script>
-import { getList } from '@/api/table'
+import { getList } from "@/api/table";
 
 export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
+        published: "success",
+        draft: "gray",
+        deleted: "danger"
+      };
+      return statusMap[status];
     }
   },
   data() {
     return {
       list: null,
-      listLoading: true
-    }
+      listLoading: true,
+      centerDialogVisible: false
+    };
   },
   created() {
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
     fetchData() {
-      this.listLoading = true
+      this.listLoading = true;
       getList(this.listQuery).then(response => {
-        this.list = response.data.items
-        this.listLoading = false
+        this.list = response.data.items;
+        this.listLoading = false;
+      });
+    },
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    },
+    del() {
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     }
   }
-}
+};
 </script>
