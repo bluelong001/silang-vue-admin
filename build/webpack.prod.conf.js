84a27991e35869a2d60a195bfab6424167fc93da
bluelong <bluelong001@outlook.com>
2019-05-02 11:41:57 +0800

    fix

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -55,7 +55,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
-      title: 'vue-admin-template',
+      title: 'silang-admin',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
