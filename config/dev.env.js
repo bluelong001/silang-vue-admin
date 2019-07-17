86ce36847178fb1c0bb5dcedb5e059cef23a4ea6
Blue <36355363+bluelong001@users.noreply.github.com>
2019-07-17 16:22:02 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://wjb666.top:8085/api"',
+  BASE_API: '"http://developer-server:"',
 })
